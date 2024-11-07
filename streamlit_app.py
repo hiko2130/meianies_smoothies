3345fb225db8c664fb1337e9b0d585e1b875436d
hiko2130 <amano.hikono.5150@members.co.jp>
2024-11-07 13:58:32 +0900

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
     """
 )
 
-# import streamlit as st（上に書いてるから使わない）
+# import streamlit as st（上に書いてるから使わない）;
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
